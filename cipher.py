2384ddce48fe9f5becf9bc6b8c2c33a1a4334ab6
maryanhil <20211076@nbsc.edu.ph>
2024-04-11 15:44:10 +0800

    first commit

---
+++ cipher.py
@@ -0,0 +1,25 @@
+def encrypt(text, shift):
+    result = ""
+
+    for i in range(len(text)):
+        char = text[i]
+
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        else:
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+
+    return result
+
+def decrypt(text, shift):
+    return encrypt(text, 26 - shift)
+
+# Test the code
+text = "Marianne"
+shift = 3
+print("Text: " + text)
+print("Shift: " + str(shift))
+print("Cipher: " + encrypt(text, shift))
+
+cipher = encrypt(text, shift)
+print("Decrypted: " + decrypt(cipher, shift))
